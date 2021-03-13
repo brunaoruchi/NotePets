9c6189024d87ad4daba06c6256efb96185e27ed1
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-13 00:06:48 -0300

    Adiciona componente ImageCircle

---
+++ src/components/ImageCircle/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Image} from 'react-native';
+
+import styles from './styles';
+
+export default class ImageCircle extends Component {
+  render() {
+    const {sourceImage} = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Image source={{uri: sourceImage}} style={styles.containerImage} />
+      </View>
+    );
+  }
+}
---
+++ src/components/ImageCircle/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#D76E33',
+    alignItems: 'center',
+    borderRadius: 100,
+    borderColor: '#481610',
+    borderWidth: 1,
+  },
+  containerImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    aspectRatio: 1,
+  },
+});
